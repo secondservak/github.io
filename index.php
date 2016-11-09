aa38008b2a6f09a84c0b30415120838d6c686adc
secondservak <secondservak@gmail.com>
2016-11-09 10:53:04 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,33 @@
 <html>
  <head>
-  <title>Тестируем PHP</title>
+  <title>Тестируем PUSH на разделенном сервере</title>
+      
  </head>
  <body>
- <?php echo '<p>Привет, мир!</p>'; ?>
+  
+
+
+
+ <p>Привет, мир!</p>
+ 
+  <p>
+<h1>
+<input class="sp_push_custom_data" type="text" name="name1" placeholder="name1">
+<input class="sp_push_custom_data" type="text" name="email" placeholder="email">
+
+<input type=submit value="Subscribe"  class="sp_notify_prompt" ></h1>
+</p>
+ 
+ <h1><a href="link1.html">ссылка1</a></h1> <br>
+ <h1><a href="link2.html">ссылка2</a></h1> <br>
+ <h1><a href="link3.html">ссылка3</a></h1>
+
+ 
+ 
+             <center>
+            ыждлфваожфывао
+            </center>
+ 
+ 
  </body>
 </html>
